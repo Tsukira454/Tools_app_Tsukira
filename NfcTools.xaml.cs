b8918a425945a0c15ca9fb2cea8a9e3adfd88d02
Elvin Gamecraft <elvingamecraft@hotmail.com>
2024-12-12 21:35:41 +0100

    BASE NFC MENU

---
+++ NfcTools.xaml.cs
@@ -0,0 +1,30 @@
+namespace Tools_app_Tsukira;
+
+public partial class NfcTools : ContentPage
+{
+	public NfcTools()
+	{
+		InitializeComponent();
+	}
+
+    private void OnReadNfcClicked(object sender, EventArgs e)
+    {
+        // Action pour lire un tag NFC
+    }
+
+    private void OnWriteNfcClicked(object sender, EventArgs e)
+    {
+        // Action pour écrire sur un tag NFC
+    }
+
+    private void OnEmulateNfcClicked(object sender, EventArgs e)
+    {
+        // Action pour émuler un tag NFC
+    }
+
+    private void OnSettingsClicked(object sender, EventArgs e)
+    {
+        // Action pour ouvrir les paramètres NFC
+    }
+
+}
